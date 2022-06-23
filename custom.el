e31c3b3d9dc7390215eae49d93cdcea906a3b495
vugomars <vugomars@gmail.com>
2022-06-23 08:38:17 +0700

    Fix custom.el

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
  '(package-selected-packages
-   '(toc-org org-make-toc js-react-redux-yasnippets rjsx-mode pdf-tools hackernews lsp-ui ewal-doom-themes doom-modeline doom afternoon-theme)))
+   '(rainbow-mode zenity-color-picker ob-solidity solidity-flycheck solidity-mode flycheck-rust parinfer-rust-mode rust-playground rust-auto-use rustic toc-org org-make-toc js-react-redux-yasnippets rjsx-mode pdf-tools hackernews lsp-ui ewal-doom-themes doom-modeline doom afternoon-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
